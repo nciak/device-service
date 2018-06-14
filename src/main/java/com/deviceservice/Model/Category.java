7110243f4daf3aa4cd030ba596175ccba25f279e
Norbert Ciak <norbi23pl@gmail.com>
2018-06-14 20:06:48 +0200

    up3

---
+++ src/main/java/com/deviceservice/Model/Category.java
@@ -0,0 +1,50 @@
+package com.deviceservice.Model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "CATEGORY")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(targetEntity=Device.class,mappedBy="category",cascade={CascadeType.ALL},orphanRemoval=true)
+    private List<Device> devices;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Device> getDevices() {
+        return devices;
+    }
+
+    public void setDevices(List<Device> devices) {
+        this.devices = devices;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
