0e6734e67afafd5a73022e5f215aa3fb36197059
Norbert Ciak <norbi23pl@gmail.com>
2018-06-15 22:35:47 +0200

    added parameters

---
+++ src/main/java/com/deviceservice/Controller/ParameterController.java
@@ -0,0 +1,39 @@
+package com.deviceservice.Controller;
+
+import com.deviceservice.Model.Parameter;
+import com.deviceservice.Repository.ParameterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/parameters/")
+public class ParameterController {
+
+    @Autowired
+    private ParameterRepository parameterRepository;
+
+
+
+    @RequestMapping("/device-{id}")
+    public ModelAndView getParameters(@PathVariable("id") Long deviceId) {
+        return getParameterListView(deviceId);
+    }
+
+
+    @PostMapping("/add")
+    public ModelAndView saveParameter(@ModelAttribute("parameter") Parameter parameter)
+    {
+        parameterRepository.save(parameter);
+        return new ModelAndView("redirect:/parameters/device-" + parameter.getDevice().getId());
+    }
+
+    private ModelAndView getParameterListView(Long deviceId) {
+        return new ModelAndView("param", "parameters",
+                parameterRepository.findParametersByDeviceId(deviceId));
+    }
+}
